a8f3622365f8268cb8eea1024bcd7efdf6f7b61d
Hayato Saito <omimisama@gmail.com>
2023-05-09 07:33:12 +0900

    fix render file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-# bundle install
-# bundle exec rails assets:precompile
-# bundle exec rails assets:clean
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
 # bundle exec rails db:migrate
 
-# bundle exec rails db:migrate:reset
-# bundle exec rails db:seed
-
-bundle install && yarn install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rails webpacker:compile && bundle exec rake db:migrate
+bundle exec rails db:migrate:reset
+bundle exec rails db:seed
