70f2676f809f3d7730278b83e3de52f405190904
Hayato Saito <omimisama@gmail.com>
2023-05-09 02:09:33 +0900

    fix render file

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module HiyariSystem
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # デプロイするサービスのホストを追加する
++   config.hosts << 'https://hiyari-system-ver2.onrender.com'
   end
 end
