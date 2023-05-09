d3608df0e5fdf8da21124fb046997991d42bd222
Hayato Saito <omimisama@gmail.com>
2023-05-09 17:21:27 +0900

    fix to heroku

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module HiyariSystem
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
+    config.assets.initialize_on_precompile = false
     #config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
     #config.active_record.default_timezone = :utc
