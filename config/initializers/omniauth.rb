43824bd72267969bee451a880ce633779e6ee642
Hayato Saito <omimisama@gmail.com>
2023-05-12 02:15:41 +0900

    fix credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-          Rails.application.credentials.google[:client_id],
-          Rails.application.credentials.google[:client_secret]
+            Rails.application.credentials.google[:client_id],
+            Rails.application.credentials.google[:client_secret]
 end
